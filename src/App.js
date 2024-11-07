b2ff93c1e9f066fb0182b7ebc8a921dcca923edc
hoangtu2k <thocodeonline@gmail.com>
2024-11-07 16:15:24 +0700

    tích hợp bootrap 460

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <h1>heeleo</h1>
+    <div className="container">
+      <h1 className="text-primary">Chào mừng đến với React và Bootstrap 4.6.0!</h1>
+      <Button variant="success">Nhấn vào đây</Button>
+    </div>
   );
 }
 
