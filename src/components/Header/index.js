9ced57a0878359cab92c03a446294f546446d1b2
hoangtu2k <thocodeonline@gmail.com>
2024-11-07 21:03:27 +0700

    setting header

---
+++ src/components/Header/index.js
@@ -0,0 +1,335 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import Button from "@mui/material/Button";
+import { MdMenuOpen } from "react-icons/md";
+import { MdOutlineMenu } from "react-icons/md";
+import SearchBox from "../SearchBox";
+import { CiLight } from "react-icons/ci";
+import { MdDarkMode } from "react-icons/md";
+import { IoShieldHalfSharp } from "react-icons/io5";
+import { IoCartOutline } from "react-icons/io5";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaRegBell } from "react-icons/fa6";
+
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Logout from "@mui/icons-material/Logout";
+import { Divider } from "@mui/material";
+
+const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [isOpenNotificationsDrop, setisOpenNotificationsDrop] = useState(false);
+  const openMyAcc = Boolean(anchorEl);
+  const openNotifications = Boolean(isOpenNotificationsDrop);
+
+  const handleOpenMyAccDrop = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMyAccDrop = (event) => {
+    setAnchorEl(null);
+  };
+
+  const handleOpenNotificationsDrop = () => {
+    setisOpenNotificationsDrop(true);
+  };
+
+  const handleCloseNotificationsDrop = () => {
+    setisOpenNotificationsDrop(false);
+  };
+
+  return (
+    <>
+      <header className="d-flex align-items-center">
+        <div className="container-fluid w-100">
+          <div className="row d-flex align-items-center w-100">
+            {/*Logo wraooer */}
+            <div className="col-sm-2 part1">
+              <Link to={"/"} className="d-flex align-items-center logo">
+                <img src={logo} />
+                <span className="ml-2">HOTASH</span>
+              </Link>
+            </div>
+
+            <div className="col-sm-3 d-flex align-items-center part2 pl-4">
+              <Button className="rounded-circle mr-3">
+                <MdMenuOpen />
+              </Button>
+              <SearchBox />
+            </div>
+
+            <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
+              <Button className="rounded-circle mr-3">
+                <CiLight />
+              </Button>
+
+              <Button className="rounded-circle mr-3">
+                <IoCartOutline />
+              </Button>
+
+              <Button className="rounded-circle mr-3">
+                <MdOutlineMailOutline />
+              </Button>
+
+              <div className="dropdownWrapper position-relative">
+                <Button
+                  className="rounded-circle mr-3"
+                  onClick={handleOpenNotificationsDrop}
+                >
+                  <FaRegBell />
+                </Button>
+                <Menu
+                  anchorEl={isOpenNotificationsDrop}
+                  className="notifications dropdown_list"
+                  id="notifications"
+                  open={openNotifications}
+                  onClose={handleCloseNotificationsDrop}
+                  onClick={handleCloseNotificationsDrop}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                >
+                  <div className="head pl-3 pb-0">
+                    <h4>Orders (12)</h4>
+                  </div>
+                  <Divider className="mb-1" />
+
+                  <div className="scroll">
+                    <MenuItem onClick={handleCloseNotificationsDrop}>
+                      
+                      <div className="d-flex">
+                        <div>
+                          <div className="userImg">
+                            <span className="rounded-circle">
+                              <img src="https://techvccloud.mediacdn.vn/280518386289090560/2024/9/17/reactjs-1726545361892465400796-6-0-465-817-crop-17265453645351178455990.jpg" />
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="dropdownInfo">
+                          <h4>
+                            <span>
+                              <b>Mahmudul</b>
+                              added to his favorite list
+                              <b>Leather belt steve madden</b>
+                            </span>
+                          </h4>
+                          <p className="text-sky mb-0">few seconds ago</p>
+                        </div>
+
+                      </div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNotificationsDrop}>
+                      
+                      <div className="d-flex">
+                        <div>
+                          <div className="userImg">
+                            <span className="rounded-circle">
+                              <img src="https://techvccloud.mediacdn.vn/280518386289090560/2024/9/17/reactjs-1726545361892465400796-6-0-465-817-crop-17265453645351178455990.jpg" />
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="dropdownInfo">
+                          <h4>
+                            <span>
+                              <b>Mahmudul</b>
+                              added to his favorite list
+                              <b>Leather belt steve madden</b>
+                            </span>
+                          </h4>
+                          <p className="text-sky mb-0">few seconds ago</p>
+                        </div>
+
+                      </div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNotificationsDrop}>
+                      
+                      <div className="d-flex">
+                        <div>
+                          <div className="userImg">
+                            <span className="rounded-circle">
+                              <img src="https://techvccloud.mediacdn.vn/280518386289090560/2024/9/17/reactjs-1726545361892465400796-6-0-465-817-crop-17265453645351178455990.jpg" />
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="dropdownInfo">
+                          <h4>
+                            <span>
+                              <b>Mahmudul</b>
+                              added to his favorite list
+                              <b>Leather belt steve madden</b>
+                            </span>
+                          </h4>
+                          <p className="text-sky mb-0">few seconds ago</p>
+                        </div>
+
+                      </div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNotificationsDrop}>
+                      
+                      <div className="d-flex">
+                        <div>
+                          <div className="userImg">
+                            <span className="rounded-circle">
+                              <img src="https://techvccloud.mediacdn.vn/280518386289090560/2024/9/17/reactjs-1726545361892465400796-6-0-465-817-crop-17265453645351178455990.jpg" />
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="dropdownInfo">
+                          <h4>
+                            <span>
+                              <b>Mahmudul</b>
+                              added to his favorite list
+                              <b>Leather belt steve madden</b>
+                            </span>
+                          </h4>
+                          <p className="text-sky mb-0">few seconds ago</p>
+                        </div>
+
+                      </div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNotificationsDrop}>
+                      
+                      <div className="d-flex">
+                        <div>
+                          <div className="userImg">
+                            <span className="rounded-circle">
+                              <img src="https://techvccloud.mediacdn.vn/280518386289090560/2024/9/17/reactjs-1726545361892465400796-6-0-465-817-crop-17265453645351178455990.jpg" />
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="dropdownInfo">
+                          <h4>
+                            <span>
+                              <b>Mahmudul</b>
+                              added to his favorite list
+                              <b>Leather belt steve madden</b>
+                            </span>
+                          </h4>
+                          <p className="text-sky mb-0">few seconds ago</p>
+                        </div>
+
+                      </div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNotificationsDrop}>
+                      
+                      <div className="d-flex">
+                        <div>
+                          <div className="userImg">
+                            <span className="rounded-circle">
+                              <img src="https://techvccloud.mediacdn.vn/280518386289090560/2024/9/17/reactjs-1726545361892465400796-6-0-465-817-crop-17265453645351178455990.jpg" />
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className="dropdownInfo">
+                          <h4>
+                            <span>
+                              <b>Mahmudul</b>
+                              added to his favorite list
+                              <b>Leather belt steve madden</b>
+                            </span>
+                          </h4>
+                          <p className="text-sky mb-0">few seconds ago</p>
+                        </div>
+
+                      </div>
+                    </MenuItem>
+                  </div>
+
+                <div className="pl-3 pr-3 w-100 pt-2 pb-1">
+                 <Button className="btn-blue w-100">View all Notifications</Button>
+                </div>
+
+                </Menu>
+
+              </div>
+
+              <div className="myAccWrapper">
+                <Button
+                  className="myAcc d-flex align-items-center"
+                  onClick={handleOpenMyAccDrop}
+                >
+                  <div className="userImg">
+                    <span className="rounded-circle">
+                      <img src="https://techvccloud.mediacdn.vn/280518386289090560/2024/9/17/reactjs-1726545361892465400796-6-0-465-817-crop-17265453645351178455990.jpg" />
+                    </span>
+                  </div>
+
+                  <div className="userInfo">
+                    <h4>Phung Hoang Tu</h4>
+                    <p className="mb-0">@rinkuv37</p>
+                  </div>
+                </Button>
+                <Menu
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={openMyAcc}
+                  onClose={handleCloseMyAccDrop}
+                  onClick={handleCloseMyAccDrop}
+                  slotProps={{
+                    paper: {
+                      elevation: 0,
+                      sx: {
+                        overflow: "visible",
+                        filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                        mt: 1.5,
+                        "& .MuiAvatar-root": {
+                          width: 32,
+                          height: 32,
+                          ml: -0.5,
+                          mr: 1,
+                        },
+                        "&::before": {
+                          content: '""',
+                          display: "block",
+                          position: "absolute",
+                          top: 0,
+                          right: 14,
+                          width: 10,
+                          height: 10,
+                          bgcolor: "background.paper",
+                          transform: "translateY(-50%) rotate(45deg)",
+                          zIndex: 0,
+                        },
+                      },
+                    },
+                  }}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                >
+                  <MenuItem onClick={handleCloseMyAccDrop}>
+                    <ListItemIcon>
+                      <PersonAdd fontSize="small" />
+                    </ListItemIcon>
+                    My account
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseMyAccDrop}>
+                    <ListItemIcon>
+                      <IoShieldHalfSharp fontSize="small" />
+                    </ListItemIcon>
+                    Reset Password
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseMyAccDrop}>
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,10 +3,7 @@ import { Button } from 'react-bootstrap';
 const Dashboard = () => {
   return (
     <div className="container">
-      <h1 className="text-primary">
-        Chào mừng đến với React và Bootstrap 4.6.0!
-      </h1>
-      <Button variant="success">Nhấn vào đây</Button>
+ 
     </div>
   );
 };
