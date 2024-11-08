eb5b6d4a89f838b7e648537543fb68761e759ae9
hoangtu2k <thocodeonline@gmail.com>
2024-11-08 20:12:10 +0700

    update

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,176 @@
-import { Button } from 'react-bootstrap';
+import DashboardBox from "./components/dashboardBox";
+import { FaUserCircle } from "react-icons/fa";
+import { IoMdCart } from "react-icons/io";
+import { MdShoppingBag } from "react-icons/md";
+import { GiStarsStack } from "react-icons/gi";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import { IoIosTimer } from "react-icons/io";
+import Button from "@mui/material/Button";
+import { HiDotsVertical } from "react-icons/hi";
+import { Chart } from "react-google-charts";
 
-const Dashboard = () => {
-  return (
-    <div className="container">
- 
-    </div>
-  );
+import InputLabel from "@mui/material/InputLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+export const data = [
+  ["Year", "Sales", "Expenses"],
+  ["2013", 100, 400],
+  ["2014", 1170, 460],
+  ["2015", 660, 1120],
+  ["2016", 1030, 540],
+];
+
+export const options = {
+  backgroundColor: "transparent",
+  chartArea: { width: "100%", height: "100%" },
 };
 
+const Dashboard = () => {
+  const ITEM_HEIGHT = 48;
+
+  const [showBy, setshowBy] = useState("");
+  const [showBysetCatBy, setCatBy] = useState("");
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <div className="right-content w-100">
+        <div className="row dashboardBoxWrapperRow">
+          <div className="col-md-8">
+            <div className="dashboardBoxWrapper d-flex">
+              <DashboardBox
+                color={["#1da256", "#48d48a"]}
+                icon={<FaUserCircle />}
+                grow={true}
+              />
+              <DashboardBox
+                color={["#c012e2", "#eb64fe"]}
+                icon={<IoMdCart />}
+              />
+              <DashboardBox
+                color={["#2c78e5", "#60aff5"]}
+                icon={<MdShoppingBag />}
+              />
+              <DashboardBox
+                color={["#e1950e", "#f3cd29"]}
+                icon={<GiStarsStack />}
+              />
+            </div>
+          </div>
+
+          <div className="col-md-4 pl-0">
+            <div className="box graphBox">
+              <div className="d-flex align-items-center bottomEle">
+                <h6 className="text-white mb-0 mt-0">Total Sales</h6>
+                <div className="ml-auto">
+                  <Button className="ml-auto toggleIcon" onClick={handleClick}>
+                    <HiDotsVertical />
+                  </Button>
+                  <Menu
+                    className="dropdown_menu"
+                    MenuListProps={{
+                      "aria-labelledby": "long-button",
+                    }}
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    slotProps={{
+                      paper: {
+                        style: {
+                          maxHeight: ITEM_HEIGHT * 4.5,
+                          width: "20ch",
+                        },
+                      },
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Day
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Weel
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Month
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Year
+                    </MenuItem>
+                  </Menu>
+                </div>
+              </div>
+
+              <h3 className="text-white font-weight-bold">$3,787,681.00</h3>
+              <p className="">$3,578.90 in last month</p>
+
+              <Chart
+                chartType="PieChart"
+                width="100%"
+                height="170px"
+                data={data}
+                options={options}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="card shadow border-0 p-3 mt-4">
+          <h3 className="hd">Best Selling Products</h3>
+
+          <div className="row cardFilters mt-3">
+            <div className="col-md-3">
+              <h4>SHOW BY</h4>
+
+              <Select
+                value={showBy}
+                onChange={(e)=>setshowBy(e.target.value)}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+                className="w-100"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </div>
+            <div className="col-md-3">
+              <h4>CATEGORY BY</h4>
+
+              <Select
+                value={showBysetCatBy}
+                onChange={(e)=>setCatBy(e.target.value)}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+                className="w-100"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </div>         
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
